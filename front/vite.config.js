223d92c422f27492ee4427bf197437431ea895db
F-KZ <supreme-soul@hotmail.fr>
2024-01-29 22:59:58 +0100

    deploy

--- front/vite.config.js
+++ front/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base :"/vite-deploy/",
+  base :"/Twitch-Clone/",
   server: {
     https : {
       key : './project-1-privateKey.key',
