ad6d4436004f7262af8f53b5bc20ee2cee42a3b3
F-KZ <supreme-soul@hotmail.fr>
2025-06-07 17:03:36 +0200

    api change

--- front/src/api/api.jsx
+++ front/src/api/api.jsx
@@ -4,7 +4,7 @@ export let apiTwitch =
     axios.create({
       headers : {
         'Client-ID' : `vsr11pna78ii6rgag9h1ctb7hf7led`,
-        'Authorization': 'Bearer qnxj308q34yitfsc8f9n514rqqyu2r',
+        'Authorization': 'Bearer er8g2swj2dtzm9x4y1n5m1uplg89bg',
       }
     }) 
   
@@ -12,6 +12,8 @@ export let apiTwitch =
   /*
     CLIENT_ID = 'vsr11pna78ii6rgag9h1ctb7hf7led';
     REDIRECT = 'http://127.0.0.1/';
+
+    pour raffraichir le token met le lien en dessous dans la barre url et remplace l'ancien token par le nouveau que tu tu trouveras dans l'url juste devant le &scope
 
     LIEN AUTH = https://id.twitch.tv/oauth2/authorize?client_id=vsr11pna78ii6rgag9h1ctb7hf7led&redirect_uri=https://localhost:5173/&response_type=token
 
